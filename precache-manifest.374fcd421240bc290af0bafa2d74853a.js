9c221235d7bd30188446b2026c5124e741f8b3e8
Miraj <mirajholmes@gmail.com>
2019-08-22 05:28:27 +0530

    Updates

---
+++ precache-manifest.374fcd421240bc290af0bafa2d74853a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6ae0f0e08390c6019514f53815f21b",
+    "url": "/azure-voting-client/index.html"
+  },
+  {
+    "revision": "4ed3500fbbc981c42bb7",
+    "url": "/azure-voting-client/static/js/2.8a44c26c.chunk.js"
+  },
+  {
+    "revision": "1564b4d3954048b64160",
+    "url": "/azure-voting-client/static/js/main.956f51ad.chunk.js"
+  },
+  {
+    "revision": "6a47a86aab90a0f08095",
+    "url": "/azure-voting-client/static/js/runtime~main.628c80c6.js"
+  },
+  {
+    "revision": "b1f1612611b1f9e1dc1e1c5e99c41723",
+    "url": "/azure-voting-client/static/media/profile.b1f16126.jpg"
+  }
+]);
